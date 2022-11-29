35d7a1e9d32ef9895182f68560a231e67c21f764
hafid-nda <zaeinyh@gmail.com>
2022-11-29 15:51:38 +0700

    add path resolve

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const router = require("../config/routes");
 const multer = require('multer');
+const path = require("path");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(morgan("dev"));
 /** Install JSON request parser */
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use("/file",express.static(path.join(path.resolve("./"), 'assets')));
 // app.use(bodyParser.json());
 
 /** Install Router */
