c8a97feda69148abd59d6871bd06105b39b5d7c4
hafid-nda <zaeinyh@gmail.com>
2022-12-08 13:20:48 +0700

    add mimetype

--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const util = require("util");
-const path = require('path');
+const path = require("path");
 
 const maxSize = 20 * 1024 * 1024;
 
@@ -16,7 +16,19 @@ let storage = multer.diskStorage({
 let uploadFile = multer({
   storage: storage,
   limits: { fileSize: maxSize },
-}).single("file");
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "application/pdf" ||
+      file.mimetype == "application/msword" ||
+      file.mimetype == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    ) {
+      cb(null, true); 
+    } else {
+      cb(null, false);
+      return cb(new Error("File extension must be PDF or Doc"));
+    }
+  },
+}).single("file");  
 
 let uploadFileMiddleware = util.promisify(uploadFile);
 
