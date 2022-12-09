57c0724bca0fd9f524ba1e7a4af1008a24ca9c80
hafid-nda <zaeinyh@gmail.com>
2022-12-09 13:47:32 +0700

    add peneliti

---
+++ db/migrations/20221108071442-create-peneliti.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Penelities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      namaPeneliti: {
+        type: Sequelize.STRING
+      },
+      alamatPeneliti: {
+        type: Sequelize.STRING
+      },
+      jenisKelamin: {
+        type: Sequelize.INTEGER
+      },
+      isDeleted: {
+        type: Sequelize.BOOLEAN
+      },
+      createdBy: {
+        type: Sequelize.STRING
+      },
+      updatedBy: {
+        type: Sequelize.STRING
+      },
+      deletedBy: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Penelities');
+  }
+};
